ce6525f93857834c8d7fc0623d28a391cbb06287
Dave Gavigan <davegavigan@gmail.com>
2016-01-19 19:47:14 -0600

    update(): unbundles localforage

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,6 @@ var concat = require('gulp-concat');
 
 gulp.task('build', [], function(){
 	return gulp.src([
-		'./bower_components/localforage/dist/localforage.js',
-		'./bower_components/angular-localforage/dist/angular-localForage.js',
 		'./src/dg-redmine.js'])
 		.pipe(concat('dg-redmine.js'))
 		.pipe(gulp.dest('./dist/'));
